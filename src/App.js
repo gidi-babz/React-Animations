47d00b29c0a3bbed5ab2b7e2b9eeb8e3269ca50c
Gidi Babz <tunex4ind@gmail.com>
2022-08-30 04:37:43 +0100

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
 
 import './App.css';
 import Modal from './components/Modal/Modal';
@@ -7,7 +8,7 @@ import List from './components/List/List';
 
 class App extends Component {
 	state = {
-		modalIsOpen: false,
+		modalIsOpen: true,
 	};
 
 	showModal = () => {
@@ -22,8 +23,18 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h1>React Animations</h1>
-				<Modal show={this.state.modalIsOpen} closed={this.closeModal} />
-				<Backdrop show={this.state.modalIsOpen} />
+				<Transition
+					mountOnEnter
+					unmountOnExit
+					in={this.state.modalIsOpen}
+					timeout={300}
+				>
+					{(state) => <Modal show={state} closed={this.closeModal} />}
+				</Transition>
+				{this.state.modalIsOpen ? (
+					<Backdrop show closed={this.closeModal} />
+				) : null}
+
 				<button className="Button" onClick={this.showModal}>
 					Open Modal
 				</button>
--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -8,7 +8,7 @@ const backdrop = (props) => {
 		props.show ? 'BackdropOpen' : 'BackdropClosed',
 	];
 
-	return <div className={cssClasses.join(' ')}></div>;
+	return <div className={cssClasses.join(' ')} onClick={props.closed}></div>;
 };
 
 export default backdrop;
